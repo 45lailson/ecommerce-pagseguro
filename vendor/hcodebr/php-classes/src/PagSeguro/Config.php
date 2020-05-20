eedd36287c05732f8caf397dbf3e45362b7ba4eb
teste <lailson4545@hotmail.com>
2020-05-19 22:14:00 -0300

    Configuração do projeto

---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace Hcode\PagSeguro;
+
+class Config {
+
+	const SANDBOX = true;
+
+	const SANDBOX_EMAIL = "lailson4545@hotmail.com";
+	const PRODUCTION_EMAIL = "lailson4545@hotmail.com";
+
+	const SANDBOX_TOKEN = "7015065D769A4468B8A8F2E3590253BA";
+	const PRODUCTION_TOKEN = "";
+
+	const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
+	const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
+
+	const SANDBOX_URL_JS = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+	const PRODUCTION_URL_JS = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+
+	public static function getAuthentication():array
+	{
+
+		if (Config::SANDBOX === true)
+		{
+
+			return [
+				"email"=>Config::SANDBOX_EMAIL,
+				"token"=>Config::SANDBOX_TOKEN
+			];
+
+		} else {
+
+			return [
+				"email"=>Config::PRODUCTION_EMAIL,
+				"token"=>Config::PRODUCTION_TOKEN
+			];
+		}
+	}
+
+	public static function getUrlSessions():string
+	{
+		return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
+	}
+
+	public static function getUrlJS()
+	{
+
+	}
+
+}
+
+
+ ?>
