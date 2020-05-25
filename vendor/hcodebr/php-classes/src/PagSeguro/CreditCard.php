c0c622405c5a1cafdad5afe47ba6836c6ea88f13
teste <lailson4545@hotmail.com>
2020-05-25 20:45:08 -0300

    Classe creditCard

--- vendor/hcodebr/php-classes/src/PagSeguro/CreditCard.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/CreditCard.php
@@ -9,6 +9,61 @@ class CreditCard {
 	private $holder;
 	private $billingAddress;
 
+	public function __construct(
+		string $token,
+		installment $installment,
+		Holder $holder,
+		Address $billingAddress
+		) 
+	{
+
+		if (!$token)
+		{
+
+			throw new Exception("Informe o token do cartão de Credito.");
+			
+		}
+
+
+		$this->token = $token;
+		$this->installment = $installment;
+		$this->holder = $holder;
+		$this->billingAddress = $billingAddress;
+
+	}
+
+	public function getDOMElement():DOMElement
+	{
+
+		$dom = new DOMDocument();
+
+		$CreditCard = $dom->createElement("CreditCard");
+		$CreditCard = $dom->appendChild($CreditCard);
+
+		$token = $dom->createElement("token", $this->token);
+		$token = $CreditCard->appendChild($token);
+
+
+		$installment =  $this->installment->getDOMElement();
+		$installment = $dom->importNode($installment, true);
+		$installment = $creditCard->appendChild($installment);
+
+		$holder =  $this->holder->getDOMElement();
+		$holder = $dom->importNode($holder, true);
+		$holder = $creditCard->appendChild($holder);
+
+
+		$billingAddress =  $this->billingAddress->getDOMElement("billingAddress");
+		$billingAddress = $dom->importNode($billingAddress, true);
+		$billingAddress = $creditCard->appendChild($billingAddress);
+		
+
+		return $CreditCard;
+
+	}
+
+	
+
 }
 
 
