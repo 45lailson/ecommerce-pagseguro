5c1e2344b3ec03fc4c93b05799a0080a901d66ee
teste <lailson4545@hotmail.com>
2020-05-25 19:30:51 -0300

    Classe Installment

--- vendor/hcodebr/php-classes/src/PagSeguro/CreditCard/Installment.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/CreditCard/Installment.php
@@ -6,6 +6,57 @@ class Installment {
 
 	private $quantity;
 	private $value;
+
+	public function __construct(
+		int $quantity,
+		float $value
+
+	)
+	{
+
+		if ($quantity < 1 || $quantity > Config::MAX_INSTALLMENT)
+		{
+
+			throw new Exception("Número de parcelas inválida.");
+			
+		}
+
+		if ($value <= 0)
+		{
+
+			throw new Exception("Valor total inválido.");
+			
+		}
+
+		$this->quantity = $quantity;
+		$this->value = $vallue;
+
+
+	}
+
+	 public function getDOMElement():DOMElement
+
+	{
+
+		$dom = new DOMDocument();
+
+		$installment = $dom->createElement("installment");
+		$installment = $dom->appendChild($installment);
+
+		
+		$value = $dom->createElement("value", number_format($this->value, 2, ".", ""));
+		$value = $type->appendChild($value);
+
+		$quantity = $dom->createElement("quantity", $this->quantity);
+		$quantity = $quantity->appendChild($quantity);
+
+		$noInterestInstallmentQuantity = $dom->createElement("noInterestInstallmentQuantity", Config::MAX_INSTALLMENT_NO_INTEREST);
+		$noInterestInstallmentQuantity = $noInterestInstallmentQuantity->appendChild($noInterestInstallmentQuantity);
+
+
+		return $installment;
+
+	}
 	
 }
 
